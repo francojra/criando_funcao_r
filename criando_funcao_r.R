32eb5867660b92e13e9fce05c2de544de8de467c
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-01-11 18:57:35 -0300

    Script

--- criando_funcao_r.R
+++ criando_funcao_r.R
@@ -4,11 +4,11 @@
 ## Função de segundo grau: f(x) = ax² + bx + c
 
 fsg <- function(a, b, c) {
-  res <- a^2 + b + c + 3 * 5 
+  res <- a^2 + b + c - 1 + 3 * 5 
   return(res)
 }
 
-## Usando os valores de a, b e c, o resultado retorna o valor da função de segundo grau
+## Usando os valores de a, b e c, a função criada retorna o resultado
 ### Exemplo: a = 1, b = 1, c = 6
 
 fsg(1, 1, 6)
