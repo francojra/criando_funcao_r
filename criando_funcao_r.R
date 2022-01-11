b25bcbbcc66e684fef40416b3508f29ac726f6e4
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-01-11 19:29:46 -0300

    Script

--- criando_funcao_r.R
+++ criando_funcao_r.R
@@ -8,7 +8,7 @@ fsg <- function(a, b, c, d, e, f) {
   return(res)
 }
 
-## Usando os valores de a, b e c, a função criada retorna o resultado
+## Usando os valores de a, b, c, d, e e f a função criada retorna o resultado
 ### Exemplo: a = 1, b = 1, c = 6, d = 3, e = 5, f = 4
 
 fsg(1, 1, 6, 3, 5, 4)
